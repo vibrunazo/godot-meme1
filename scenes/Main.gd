2f5b370345484d34cef73ed2b8f55040c62ff15a
Vandré Brunazo <vibrunazo@gmail.com>
2022-11-15 23:00:43 -0300

    animate text

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -8,13 +8,17 @@ func _ready():
 
 func _on_Button1_pressed():
 	print('b1')
+	$AnimationPlayer.play("1")
 
 func _on_Button2_pressed():
 	print('b2')
+	$AnimationPlayer.play("2")
 
 
 func _on_Button3_pressed():
 	print('b3')
+	$AnimationPlayer.play("3")
 
 func _on_Button4_pressed():
 	print('b4')
+	$AnimationPlayer.play("4")
