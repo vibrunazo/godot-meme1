71bd5c131817ec8f933749aff65629e00db39637
Vandré Brunazo <vibrunazo@gmail.com>
2022-11-15 22:45:16 -0300

    setup buttons

---
+++ scenes/Main.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+func _ready():
+	pass # Replace with function body.
+	print('ini')
+
+
+func _on_Button1_pressed():
+	print('b1')
+
+func _on_Button2_pressed():
+	print('b2')
+
+
+func _on_Button3_pressed():
+	print('b3')
+
+func _on_Button4_pressed():
+	print('b4')
